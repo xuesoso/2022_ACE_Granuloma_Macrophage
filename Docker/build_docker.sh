9b39080117d2f27a2b9b028aef0e788cddbfe247
xuesoso <yuanxue@stanford.edu>
2022-11-29 16:36:45 -0800

    attempts to fix groupadd error that appears on MacOS

--- Docker/build_docker.sh
+++ Docker/build_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build the docker image with the tag "py37_granuloma_scrna" and the same user name and group permission as the host user
-docker build Docker -f Docker/Dockerfile -t py37_granuloma_scrnaseq --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)
+docker build Docker -f Docker/Dockerfile -t py37_granuloma_scrnaseq --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) 
