b867ad1a110991a500a4723d8340d6b46f581b9d
xuesoso@gmail.com <xuesoso@gmail.com>
2022-11-29 16:46:49 -0800

    run different Dockerfile based on what OS shell detects. Currently only supports either Linux or MacOS.

--- Docker/build_docker.sh
+++ Docker/build_docker.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 # Build the docker image with the tag "py37_granuloma_scrna" and the same user name and group permission as the host user
-docker build Docker -f Docker/Dockerfile -t py37_granuloma_scrnaseq --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) 
+if [[ $OSTYPE == 'darwin'* ]]; then
+    echo 'detected MacOS system'
+    docker build Docker -f Docker/Dockerfile.macos -t py37_granuloma_scrnaseq --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) 
+elif [[ $OSTYPE == 'linux-gnu' ]]; then
+    echo 'detected Linux system'
+    docker build Docker -f Docker/Dockerfile.linux -t py37_granuloma_scrnaseq --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) 
+else
+    echo 'not a supported OS. Please use MacOS or Linux to build this image.'
+fi
